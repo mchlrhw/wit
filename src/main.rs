c87551ae0df0a2a6159a97507c1a40f5eb04bd75
mchlrhw <4028654+mchlrhw@users.noreply.github.com>
2022-03-07 08:46:04 +0000

    Implement a simple REPL

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,45 @@
+use std::{env, io::Write, process};
 use wit::{codegen::jit_compile, Parser};
 
-fn main() -> anyhow::Result<()> {
-    let source = "3 - 2 - 1";
+fn run(source: &str) -> anyhow::Result<()> {
     let expr = Parser::new(source).parse()?;
     let val = jit_compile(expr)?;
 
     println!("{val}");
 
     Ok(())
+}
+
+fn run_repl() -> anyhow::Result<()> {
+    loop {
+        print!("> ");
+        std::io::stdout().flush()?;
+
+        let mut line = String::new();
+        std::io::stdin().read_line(&mut line)?;
+        if line.is_empty() {
+            break;
+        }
+
+        run(&line)?;
+    }
+
+    Ok(())
+}
+
+fn run_file(_path: &str) -> anyhow::Result<()> {
+    todo!("running programs from a file is not yet implemented")
+}
+
+fn main() -> anyhow::Result<()> {
+    let args = env::args().skip(1).collect::<Vec<_>>();
+
+    match args.len() {
+        0 => run_repl(),
+        1 => run_file(&args[0]),
+        _ => {
+            println!("Usage: wit [script]");
+            process::exit(1);
+        }
+    }
 }
